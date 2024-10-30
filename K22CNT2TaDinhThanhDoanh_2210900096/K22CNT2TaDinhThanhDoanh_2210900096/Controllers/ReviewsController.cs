87909f29c02b0257a7512858cae2f527788cbe25
CatchTheRainbow04 <doanhdoe@gmail.com>
2024-10-31 00:42:22 +0700

    31/10

---
+++ K22CNT2TaDinhThanhDoanh_2210900096/K22CNT2TaDinhThanhDoanh_2210900096/Controllers/ReviewsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using K22CNT2TaDinhThanhDoanh_2210900096.Models;
+
+namespace K22CNT2TaDinhThanhDoanh_2210900096.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        private movie_websiteEntities db = new movie_websiteEntities();
+
+        // GET: Reviews
+        public ActionResult Index()
+        {
+            var reviews = db.Reviews.Include(r => r.Movies).Include(r => r.Users);
+            return View(reviews.ToList());
+        }
+
+        // GET: Reviews/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reviews reviews = db.Reviews.Find(id);
+            if (reviews == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reviews);
+        }
+
+        // GET: Reviews/Create
+        public ActionResult Create()
+        {
+            ViewBag.MovieID = new SelectList(db.Movies, "MovieID", "Title");
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "Username");
+            return View();
+        }
+
+        // POST: Reviews/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ReviewID,MovieID,UserID,Rating,Comment,ReviewDate")] Reviews reviews)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Reviews.Add(reviews);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MovieID = new SelectList(db.Movies, "MovieID", "Title", reviews.MovieID);
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "Username", reviews.UserID);
+            return View(reviews);
+        }
+
+        // GET: Reviews/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reviews reviews = db.Reviews.Find(id);
+            if (reviews == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MovieID = new SelectList(db.Movies, "MovieID", "Title", reviews.MovieID);
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "Username", reviews.UserID);
+            return View(reviews);
+        }
+
+        // POST: Reviews/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ReviewID,MovieID,UserID,Rating,Comment,ReviewDate")] Reviews reviews)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(reviews).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MovieID = new SelectList(db.Movies, "MovieID", "Title", reviews.MovieID);
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "Username", reviews.UserID);
+            return View(reviews);
+        }
+
+        // GET: Reviews/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reviews reviews = db.Reviews.Find(id);
+            if (reviews == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reviews);
+        }
+
+        // POST: Reviews/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Reviews reviews = db.Reviews.Find(id);
+            db.Reviews.Remove(reviews);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ K22CNT2TaDinhThanhDoanh_2210900096/K22CNT2TaDinhThanhDoanh_2210900096/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="K22CNT2TaDinhThanhDoanh_2210900096.MvcApplication" Language="C#" %>
